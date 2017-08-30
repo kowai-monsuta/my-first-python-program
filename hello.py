4749632afa26aaf12fdfc709ebe2569df26b0f36
kowai-monsuta <mvazqu0696@greenvilleschools.us>
2017-08-30 14:18:35 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,21 @@
+# This program says hello and greets a person by name.
+#
+# Melissa
+# August 24, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+print("Were were you born, " + name + ".")
+place_born = input()
+print("That's interesting. I'd like to visit " + place_born + " one day.")
+print("How old are you?")
+age = input()
+print("Wow! You're only " + age + " years old?")
+yes_no = input()
+print("When were you born?")
+birthday = input()
+print("Are you going to have a party on " + birthday + "?")
+yes_no = input()
+print("It was nice meeting you " + name + ".")
---
+++ madlib.py
@@ -0,0 +1,19 @@
+# This program is a madlib
+#
+# Melissa
+# August 28
+
+print=("Hello")
+print=("What is your name?")
+name = input()
+print=("How old are you?")
+age = input()
+print=("What's your favorite food?")
+food = input()
+print=("What is the name of one of your friends?")
+friend1 = input()
+print=("What is the name of your other friend?")
+friend2 = input()
+
+print=(name + " is " + age + " years old." + name + " loves to eat " + food +
+       ". One day " + name + "'s friend, " 
